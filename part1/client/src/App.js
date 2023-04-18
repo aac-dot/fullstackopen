cefb3951a893b645b6e4b9f8cee07267987a1c97
aac-dot <ayrton.andrade1@github.com>
2023-04-17 22:04:11 -0300

    define the data in App

--- part1/client/src/App.js
+++ part1/client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+const App = () => {
+  const course = 'Half stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercise1 = 10
+  const part2 = 'Using props to pass data'
+  const exercise2 = 7
+  const part3 = 'State of component'
+  const exercise3 = 14
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
+      <p>{part1}</p>
+      <p>{part2}</p>
+      <p>{part3} {exercise3}</p>
+      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
     </div>
-  );
+  )
 }
-
 export default App;
