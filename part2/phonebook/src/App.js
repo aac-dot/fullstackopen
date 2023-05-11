2354667f65b17d3f3b73c42522ca7395eb8df4ae
aac-dot <ayrton.andrade1@github.com>
2023-05-11 14:51:12 -0300

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -17,20 +17,57 @@ import './App.css';
 //   }
 // }
 
+const Person = (props) => {
+  return (
+    <div>{props.person.name}</div>
+  )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+  const [persons, setPersons] = useState([{id: 1, name: 'Arto Hellas'}])
   const [newName, setNewName] = useState('')
+
+  console.log(persons)
+
+  const IsNameAlreadyExist = function(name) {
+    let person;
+    
+    for (person of persons) {
+      if (person.name === newName) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    
+    if (IsNameAlreadyExist(newName)) {
+      alert(`${newName} is already added on phonebook`);
+    } else {
+      setPersons(persons.concat({ id: persons.length + 1, name: newName }))
+      setNewName('');
+    }
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        name: <input/>
+      <form onSubmit={addName}>
+        name: <input value={newName} onChange={handleChange}/>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <li>
+        <ul>{persons.map(person => <Person key={person.id} person={person} />)}</ul>
+      </li>
     </div>
   )
 }
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
