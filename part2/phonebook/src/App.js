636d7ff1b17d003eaec993137cda99d1353c4269
aac-dot <ayrton.andrade1@github.com>
2023-05-26 18:43:27 -0300

    end of 2.6: The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,73 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+import Person from './components/Person.js';
 
-const Person = (props) => {
-  return (
-    <div>{props.person.name}</div>
-  )
-}
+/* eslint-disable */
 
 const App = () => {
-  const [persons, setPersons] = useState([{id: 1, name: 'Arto Hellas'}])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  
   const [newName, setNewName] = useState('')
 
-  console.log(persons)
-
-  const IsNameAlreadyExist = function(name) {
-    let person;
-    
-    for (person of persons) {
-      if (person.name === newName) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  const addName = (event) => {
-    event.preventDefault()
-    
-    if (IsNameAlreadyExist(newName)) {
-      alert(`${newName} is already added on phonebook`);
-    } else {
-      setPersons(persons.concat({ id: persons.length + 1, name: newName }))
-      setNewName('');
-    }
-  }
-
-  const handleChange = (event) => {
+  const handleChangedName = (event) => {
     setNewName(event.target.value)
+    console.log(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        name: <input value={newName} onChange={handleChange}/>
+      <form>
         <div>
-          <button type="submit">Add</button>
+          name: <input 
+                  value={newName}
+                  onChange={handleChangedName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      <li>
-        <ul>{persons.map(person => <Person key={person.id} person={person} />)}</ul>
-      </li>
+      
+      <ul>
+        {persons.map(person => <Person key={person.name} person={person} /> )}
+      </ul>
     </div>
   )
 }
